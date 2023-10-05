8def47f99e54f7b3f07ae254c5eb8dd0ee9fc8b1
Srujan-github <101975044+Srujan-github@users.noreply.github.com>
2023-10-05 10:28:32 +0530

    moongese setup

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express= require('express');
-
+// const morgan=require('morgan');
 const app=express();
+const mongoose=require('mongoose');
+const Blog=require('./models/bolg')
+ const dbURI='mongodb+srv://srujan:test123@netninja-project.arviczx.mongodb.net/Netninja-project1?retryWrites=true&w=majority';
+
+ mongoose.connect(dbURI,{useNewUrlParser: true,useUnifiedTopology:true})
+ .then((result)=>console.log("connected to DB")).catch((err)=>console.log(err));
 // register view engine
 app.set('view engine','ejs');
 app.listen(3000);
@@ -8,7 +14,7 @@ app.listen(3000);
 //     console.log();
 // })
 app.use(express.static('public'));
-
+// app.use(morgan('dev'));
 //middleware
 // app.use((req,res,next)=>{
 //     console.log('new request as made');
@@ -18,6 +24,36 @@ app.use(express.static('public'));
 //     next();
 //     // console.log('new request as made');
 // })
+
+app.get('/add-blog',(req,res)=>{
+    const blog=new Blog({
+        title: 'new-blog  1',
+        snippet: 'about my new blog',
+        body: 'more abouyt my blog'
+    });
+
+    blog.save().then((result)=>{
+        res.send(result)
+    }).catch((err)=>{
+        console.log(err);
+    })
+})
+
+app.get('/all-blogs',(req,res)=>{
+      Blog.find().then((result)=>{
+        res.send(result);
+    }).catch((err)=>{
+        console.log(err);
+    })
+})
+app.get('/single-blog',(req,res)=>{
+      Blog.findById ('651daa1d89f7265b315d5872').then((result)=>{
+        res.send(result);
+    }).catch((err)=>{
+        console.log(err);
+    })
+})
+
 app.get('/',(req,res)=>{
     const blogs = [
         {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
---
+++ models/bolg.js
@@ -0,0 +1,20 @@
+const mongoose=require('mongoose');
+const Scheme=mongoose.Schema;
+
+const blogScheme=new Scheme({
+    title:{
+        type: String,
+        required:true
+        },
+    snippet:{
+        type: String,
+        required:true
+        },
+    body:{
+        type: String,
+        required:true
+        },
+},{timestamps:true});
+
+const Blog=mongoose.model('Blog',blogScheme);
+module.exports=Blog;
